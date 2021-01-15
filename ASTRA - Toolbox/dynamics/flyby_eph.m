cbe0c0ba0c2de2b621c08743662a7d79914e027d
andreabellome <67928580+andreabellome@users.noreply.github.com>
2021-01-15 10:59:25 +0100

    Update flyby_eph.m

--- ASTRA - Toolbox/dynamics/flyby_eph.m
+++ ASTRA - Toolbox/dynamics/flyby_eph.m
@@ -1,13 +1,31 @@
 function [rrOU, vvOU] = flyby_eph(rrIN, vvIN, plIN, tIN, h, gam)
 
-[~, vvPL, ~, ~, radPL, muPL] = ephj2000(plIN, tIN);
+% this function computes the flyby with a planet given its position in ephemerides model 
+% 
+% INPUT : 
+% - rrIN : position vector of the incoming leg at the planet (km)
+% - vvIN : velocity vector of the incoming leg at the planet (km/s)
+% - plIN : planet to flyby
+% - h    : flyby altitude (km)
+% - gam  : flyby hyperbola plane inclination (rad)
+% - mu   : gravitational parameter of the Sun (km3/s2)
+% 
+% OUTPUT : 
+% - rrOU : position vector of the outgoing leg at the planet (km)
+% - vvOU : velocity vector of the incoming leg at the planet (km)
 
-vvInfIN = vvIN - vvPL;
-n_r     = cross(vvInfIN,vvPL)./norm(cross(vvInfIN,vvPL));
+% TBR (Andrea) : check gamma angle
+% TBR (Andrea) : use this function or the circular coplanar one?
 
-vvInfOU = swingby(vvInfIN, (radPL + h), muPL, gam, n_r);
+[~, vvPL, ~, ~, radPL, muPL] = ephj2000(plIN, tIN); % find planet properties from ephemerides
 
-rrOU = rrIN;
-vvOU = vvInfOU' + vvPL;
+vvInfIN = vvIN - vvPL; % infinity velocity at planet encounter
+n_r     = cross(vvInfIN,vvPL)./norm(cross(vvInfIN,vvPL)); % reference vector for the flyby plane
+
+vvInfOU = swingby(vvInfIN, (radPL + h), muPL, gam, n_r); % compute the outgoing infinity velocity
+
+% outgoing parameters
+rrOU = rrIN; % outgoing position vector
+vvOU = vvInfOU' + vvPL; % outgoing velocity vector
 
 end
