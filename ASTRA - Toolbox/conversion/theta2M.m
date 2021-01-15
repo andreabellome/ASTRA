aa141ca451bdb6a5a00b85eb8aa2f0f16d3553d4
andreabellome <67928580+andreabellome@users.noreply.github.com>
2021-01-15 10:36:57 +0100

    Update theta2M.m

--- ASTRA - Toolbox/conversion/theta2M.m
+++ ASTRA - Toolbox/conversion/theta2M.m
@@ -9,12 +9,12 @@ function M = theta2M(theta, e)
 % OUTPUT:
 % - M : mean anomaly (rad)
 
-if e < 1
-E = 2*atan(sqrt((1-e)/(1+e))*tan(theta/2));
-M =  E - e*sin(E);
-elseif e > 1
-E = 2*atanh(sqrt((e-1)/(e+1))*tan(theta/2));
-M = e*sinh(E) - E;
+if e < 1 % elliptical orbits
+E = 2*atan(sqrt((1-e)/(1+e))*tan(theta/2)); % from true to eccentric anomaly
+M =  E - e*sin(E); % from eccentric to mean anomaly
+elseif e > 1 % parabolic and hyperbolic
+E = 2*atanh(sqrt((e-1)/(e+1))*tan(theta/2));  % from true to eccentric anomaly
+M = e*sinh(E) - E; % from eccentric to mean anomaly
 end
 
 end
