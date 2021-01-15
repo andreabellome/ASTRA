2c1e7e24eb51ba924f3eca594d51554beaf347b5
andreabellome <67928580+andreabellome@users.noreply.github.com>
2021-01-15 10:35:23 +0100

    Update theta2M.m

--- ASTRA - Toolbox/conversion/theta2M.m
+++ ASTRA - Toolbox/conversion/theta2M.m
@@ -1,5 +1,14 @@
 function M = theta2M(theta, e)
 
+% this function computes the mean anomaly from true anomaly and eccentricity
+% 
+% INPUT : 
+% - theta : true anomaly (rad)
+% - e     : eccentricity
+% 
+% OUTPUT:
+% - M : mean anomaly (rad)
+
 if e < 1
 E = 2*atan(sqrt((1-e)/(1+e))*tan(theta/2));
 M =  E - e*sin(E);
