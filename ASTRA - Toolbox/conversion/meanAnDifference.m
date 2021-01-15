988eb529d51f7054f174e913de9a1be8fa594377
andreabellome <67928580+andreabellome@users.noreply.github.com>
2021-01-15 10:34:11 +0100

    Update meanAnDifference.m

--- ASTRA - Toolbox/conversion/meanAnDifference.m
+++ ASTRA - Toolbox/conversion/meanAnDifference.m
@@ -1,5 +1,13 @@
 function [DM] = meanAnDifference(M1, M2)
 
+% this function computes the anomaly difference between M1 and M2
+%
+% INPUT : 
+% - M1 : first anomaly (rad)
+% - M2 : second anomaly (rad)
+
+% TBR (Andrea) -> maybe letting DM to be negative would be beneficial for non linear solver
+
 if abs(M1 - M2) > pi
     DM = 2*pi - abs(M1 - M2);
 else
