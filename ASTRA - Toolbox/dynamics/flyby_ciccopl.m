dd60979e5d2038a5d45fc8ad578007a8dd3d6608
andreabellome <67928580+andreabellome@users.noreply.github.com>
2021-01-15 10:53:06 +0100

    Update flyby_ciccopl.m

--- ASTRA - Toolbox/dynamics/flyby_ciccopl.m
+++ ASTRA - Toolbox/dynamics/flyby_ciccopl.m
@@ -1,14 +1,31 @@
 function [rrOU, vvOU] = flyby_ciccopl(rrIN, vvIN, plIN, h, gam, mu)
 
-[rPL, radPL, muPL] = astroConstantsj2000(plIN);
+% this function computes the circular-coplanar flyby
+% 
+% INPUT : 
+% - rrIN : position vector of the incoming leg at the planet (km)
+% - vvIN : velocity vector of the incoming leg at the planet (km/s)
+% - plIN : planet to flyby
+% - h    : flyby altitude (km)
+% - gam  : flyby hyperbola plane inclination (rad)
+% - mu   : gravitational parameter of the Sun (km3/s2)
+% 
+% OUTPUT : 
+% - rrOU : position vector of the outgoing leg at the planet (km)
+% - vvOU : velocity vector of the incoming leg at the planet (km)
 
-vvPL    = sqrt(mu/rPL).*[-sin(MeanAn(rrIN)) cos(MeanAn(rrIN)) 0];
-vvInfIN = vvIN - vvPL;
-n_r     = cross(vvInfIN,vvPL)./norm(cross(vvInfIN,vvPL));
+% TBR (Andrea) : check gamma angle
 
-vvInfOU = swingby(vvInfIN, (radPL + h), muPL, gam, n_r);
+[rPL, radPL, muPL] = astroConstantsj2000(plIN); % physical constants of the given planet
 
-rrOU = rrIN;
-vvOU = vvInfOU' + vvPL;
+vvPL    = sqrt(mu/rPL).*[-sin(MeanAn(rrIN)) cos(MeanAn(rrIN)) 0]; % planet heliocentric velocity vector
+vvInfIN = vvIN - vvPL; % infinity velocity at the planet encounter
+n_r     = cross(vvInfIN,vvPL)./norm(cross(vvInfIN,vvPL)); % reference vector for the flyby plane
+
+vvInfOU = swingby(vvInfIN, (radPL + h), muPL, gam, n_r); % compute the outgoing infinity velocity
+
+% outgoing parameters
+rrOU = rrIN; % outgoing position vector
+vvOU = vvInfOU' + vvPL; % outgoing velocity vector
 
 end
