a6b4427288b4dfe38e03aad565932514ed134019
andreabellome <67928580+andreabellome@users.noreply.github.com>
2021-01-15 10:32:06 +0100

    Update MeanAn.m

--- ASTRA - Toolbox/conversion/MeanAn.m
+++ ASTRA - Toolbox/conversion/MeanAn.m
@@ -1,8 +1,18 @@
 function [MPL] = MeanAn(rrPL)
 
+% this function computes the mean anomaly from the position vector
+%
+% INPUT : 
+% - rrPL : planet position vector (km)
+%
+% OUTPUT : 
+% - MPL : mean anomaly (rad)
+
+% read the position vector components
 xPL = rrPL(1);
 yPL = rrPL(2);
 
+% find mean anomaly and wrap to 2pi
 MPL = atan2(yPL, xPL);
 MPL = wrapTo2Pi(MPL);
 
